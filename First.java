545fec14fdf1dae125e5b82fdc591d353ec2b518
gabriellebillimoria <billimoria_gabrielle@students.columbusstate.edu>
2025-01-28 13:33:27 -0500

    Add files via upload

---
+++ First.java
@@ -0,0 +1,14 @@
+// Author: Gabrielle
+// Program: First.java
+// This program displays a greeting, a job title, rationale for the job, and median salary.
+
+public class First {
+    public static void main(String[] args) {
+        // Output the required information
+        System.out.println("Hello Gabrielle!");
+        System.out.println("Job title: Information Security Specialist");
+        System.out.println("Why: Because it's in the field of cybersecurity");
+        System.out.println("Median salary: $120,360");
+    }
+}
+
